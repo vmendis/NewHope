664ed7d95d18a87a780baa5955285de97a36b914
Virantha Mendis <virantha.mendis@compute.internal>
2019-06-05 14:03:11 +0200

    created buils-all.sh. this will build all docker images in one go

---
+++ Virantha-Docker/version.sh
@@ -0,0 +1,9 @@
+# Example version script.
+# Please choose one version or create your own
+
+# Node.js: grep the version from a package.json file with jq
+#jq -rM '.version' package.json
+
+cat version.sh | grep version | grep '\([0-9.]\+\.\?\)\{3\}' -o
+
+version=3.7
